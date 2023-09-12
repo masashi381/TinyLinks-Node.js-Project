180fd52af818889678c979f1de284288283e913f
michiru6828 <since_l999@yahoo.co.jp>
2023-09-12 15:43:10 -0700

    created basic url related routes

--- routes/urls.js
+++ routes/urls.js
@@ -1,4 +1,31 @@
 import express from 'express';
 const urlRouter = express.Router();
 
+urlRouter.get('/', (req, res) => {
+  res.render('urls');
+});
+
+urlRouter.post('/', (req, res) => {
+  console.log('post');
+});
+
+urlRouter.get('/new', (req, res) => {
+  res.render('newUrl');
+});
+
+urlRouter.get('/:id', (req, res) => {
+  res.render('singleUrl');
+});
+
+//??
+urlRouter.get('/u/:id', (req, res) => {});
+
+urlRouter.put('/:id', (req, res) => {
+  console.log('edited');
+});
+
+urlRouter.get('/:id/delete', (req, res) => {
+  console.log('deleted');
+});
+
 export default urlRouter;
