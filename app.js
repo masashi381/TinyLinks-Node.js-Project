e5dcbc7f70e781bfc9379fb9dec3624b39383d88
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-09-13 10:38:08 -0700

    temporary update

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ dotenv.config();
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('hello');
--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,25 @@
 import express from 'express';
 const authRouter = express.Router();
 import authJson from '../models/users.json' assert { type: 'json' };
-
-console.log(authJson);
+import { v4 as uuidv4 } from 'uuid';
 //show my login page
 authRouter.get('/', (req, res) => {
-  res.send('login');
+  res.render('login');
 });
 
 //show login page
-authRouter.get('/login', (req, res) => {
-  res.send(authJson);
-});
+// authRouter.get('/login', (req, res) => {});
 
 // show register page
 authRouter.get('/register', (req, res) => {
-  res.send('register');
+  res.render('register');
 });
 
+authRouter.post('/register', (req, res) => {
+  console.log(req.body);
+  res.send('form works');
+});
+
+const test = uuidv4();
+console.log('test:', test);
 export default authRouter;
