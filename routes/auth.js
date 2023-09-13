a8804ca85bb60e79a8b228d75f979d19cd1b9b48
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-09-12 19:59:35 -0700

    set show login & register page

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,21 @@
 import express from 'express';
 const authRouter = express.Router();
+import authJson from '../models/users.json' assert { type: 'json' };
 
+console.log(authJson);
 //show my login page
 authRouter.get('/', (req, res) => {
   res.send('login');
+});
+
+//show login page
+authRouter.get('/login', (req, res) => {
+  res.send(authJson);
+});
+
+// show register page
+authRouter.get('/register', (req, res) => {
+  res.send('register');
 });
 
 export default authRouter;
