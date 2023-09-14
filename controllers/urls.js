18818f7a5c72dfdba7a74a6c11c39f40082f42f4
MasakiFukunishi <fmsknn@gmail.com>
2023-09-13 22:16:26 -0700

    divide function

--- controllers/urls.js
+++ controllers/urls.js
@@ -5,31 +5,67 @@ import { SHORT_URL_LENGTH } from '../constants/urls.js';
 
 const filePath = path.join(path.resolve(), '/models/urls.json');
 
-export const createUrl = (req, res) => {
-  const { userId, longUrl } = req.body;
+const validateUrl = (userId, longUrl) => {
   if (!userId || !longUrl) {
-    return res.status(400).json({ error: 'userId and longUrl are required' });
+    return { valid: false, error: 'userId and longUrl are required' };
   }
-  const shortUrl = randomstring.generate(SHORT_URL_LENGTH);
+
+  const urlPattern = /^(http(s)?:\/\/)(www\.)?[\w-\/?:]+$/;
+  if (!urlPattern.test(longUrl)) {
+    return { valid: false, error: 'Invalid URL format' };
+  }
+
+  return { valid: true };
+};
+
+const readWriteFile = (filePath, callback) => {
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      return callback({ error: 'Internal Server Error' });
     }
-    let jsonData = JSON.parse(data) || {};
+    const jsonData = JSON.parse(data) || {};
+    callback(null, jsonData);
+  });
+};
+
+const writeToFile = (filePath, data, callback) => {
+  fs.writeFile(filePath, JSON.stringify(data), 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      return callback({ error: 'Internal Server Error' });
+    }
+    callback(null);
+  });
+};
+
+export const createUrl = (req, res) => {
+  const { userId, longUrl } = req.body;
+
+  const validationResult = validateUrl(userId, longUrl);
+  if (!validationResult.valid) {
+    return res.status(400).json({ error: validationResult.error });
+  }
+
+  const shortUrl = randomstring.generate(SHORT_URL_LENGTH);
+
+  readWriteFile(filePath, (err, jsonData) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
     if (!jsonData[userId]) {
       jsonData[userId] = [];
     }
-    console.log(jsonData);
+
     jsonData[userId].push({
       shortUrl,
       longUrl,
     });
-    console.log(jsonData);
-    fs.writeFile(filePath, JSON.stringify(jsonData), 'utf8', (err) => {
+
+    writeToFile(filePath, jsonData, (err) => {
       if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json(err);
       }
       res.redirect(`/urls/${shortUrl}`);
     });
