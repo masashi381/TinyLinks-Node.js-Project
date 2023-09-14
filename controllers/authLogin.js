4a45601f3ae48dc173049777f3e3fc4b929576fa
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-09-13 20:24:39 -0700

    post login info

---
+++ controllers/authLogin.js
@@ -0,0 +1,10 @@
+// post login info
+export const loginUser = (req, res) => {
+  if (req.body.email === '') {
+    console.log('Please enter your email');
+  } else if (req.body.password === '') {
+    console.log('Please enter your password');
+  } else {
+    console.log(req.body);
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ import authJson from '../models/users.json' assert { type: 'json' };
 const authRouter = express.Router();
 authRouter.use(express.json());
 import { registerUser } from '../controllers/auth.js';
+import { loginUser } from '../controllers/authLogin.js';
 
 // console.log('test:', uuid);
 
@@ -18,12 +19,19 @@ authRouter.get('/login', (req, res) => {
   res.render('login');
 });
 
+// make a POST request to /login
+authRouter.post('/login', (req, res) => {
+  loginUser(req, res);
+  res.send('login successful');
+});
+
 // show register page
 authRouter.get('/register', (req, res) => {
   console.log('results:', authJson);
   res.render('register');
 });
 
+// make a POST request to /register
 authRouter.post('/register', (req, res) => {
   // res.send('form works');
   registerUser(req, res);
