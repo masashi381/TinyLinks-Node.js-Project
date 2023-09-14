d6efcf652bbb1b0da9a3991d39dc6f0680a2db84
MasakiFukunishi <fmsknn@gmail.com>
2023-09-14 10:11:30 -0700

    make utils

--- controllers/urls.js
+++ controllers/urls.js
@@ -1,43 +1,9 @@
-import fs from 'fs';
 import path from 'path';
 import randomstring from 'randomstring';
 import { SHORT_URL_LENGTH } from '../constants/urls.js';
+import { readWriteFile, writeToFile, validateUrl } from '../helpers/utils.js';
 
 const filePath = path.join(path.resolve(), '/models/urls.json');
-
-const validateUrl = (userId, longUrl) => {
-  if (!userId || !longUrl) {
-    return { valid: false, error: 'userId and longUrl are required' };
-  }
-
-  const urlPattern = /^(http(s)?:\/\/)(www\.)?[\w-\/?:]+$/;
-  if (!urlPattern.test(longUrl)) {
-    return { valid: false, error: 'Invalid URL format' };
-  }
-
-  return { valid: true };
-};
-
-const readWriteFile = (filePath, callback) => {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return callback({ error: 'Internal Server Error' });
-    }
-    const jsonData = JSON.parse(data) || {};
-    callback(null, jsonData);
-  });
-};
-
-const writeToFile = (filePath, data, callback) => {
-  fs.writeFile(filePath, JSON.stringify(data), 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-      return callback({ error: 'Internal Server Error' });
-    }
-    callback(null);
-  });
-};
 
 export const createUrl = (req, res) => {
   const { userId, longUrl } = req.body;
